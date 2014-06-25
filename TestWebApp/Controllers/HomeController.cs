94a2337c7f7565708ed39616e52e004747215e1a
arifcse <arifcse12@gmail.com>
2014-06-25 01:02:42 -0400

    Intial Checkin

---
+++ TestWebApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "My application description page: Details";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "My contact page: 1st";
+
+            return View();
+        }
+    }
+}
